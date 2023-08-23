9de6956e5106b26f08e26c4574222a916d9b1e51
CGUltimateno <Mohameddeaf65@gmail.com>
2023-08-23 20:20:07 +0300

    first tasks

--- monty_funcs_1.c
+++ monty_funcs_1.c
@@ -61,6 +61,24 @@ void monty_push(stack_t **stack, unsigned int line_number)
  * @line_number: The current working line number of a Monty bytecodes file.
  */
 
+void monty_pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *tmp = (*stack)->next;
+
+    while (tmp)
+    {
+        printf("%d\n", tmp->n);
+        tmp = tmp->next;
+    }
+    (void)line_number;
+}
+
+/**
+ * monty_pint - Prints the top value of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+
 void monty_pint(stack_t **stack, unsigned int line_number)
 {
     if ((*stack)->next == NULL)
