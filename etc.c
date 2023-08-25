ec5147ade949409ea1cd257aa6687c713bba9f3e
CGUltimateno <Mohameddeaf65@gmail.com>
2023-08-25 19:35:36 +0300

    more stuff

--- etc.c
+++ etc.c
@@ -1,5 +1,25 @@
 #include "monty.h"
+/**
+ * print_sub_error - Prints an error message and terminates the program
+ * if the stack is too short for the "sub" operation.
+ * @line_number: Line number of the current operation.
+ * Return: Nothing
+ */
+void print_sub_error(int line_number)
+{
 
+    fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+    free_everything();
+    exit(EXIT_FAILURE);
+}
+
+
+/**
+ * is_num - Checks if a string represents a valid integer
+ * @num: The string to check
+ *
+ * Return: 1 if the string is a valid integer, 0 otherwise
+ */
 /**
  * is_num - Checks if a string represents a valid integer
  * @num: The string to check
